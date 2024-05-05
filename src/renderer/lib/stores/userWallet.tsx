0746ade3bf8dda6eeabf87f832bd596b015774d7
kadirchan <kadircanbozkurt1905@gmail.com>
2024-05-05 16:22:43 +0300

    ref userStore

--- src/renderer/lib/stores/userWallet.tsx
+++ src/renderer/lib/stores/userWallet.tsx
@@ -2,37 +2,29 @@ import { create } from 'zustand';
 
 interface UserState {
   isConnected: boolean;
-  isConnecting: boolean;
   userPublicKey?: string;
   userMinaBalance: number;
   wishlist: number[];
-  // wishlistFlag: boolean;
   library: number[];
 
   setConnected: (connected: boolean) => void;
-  setConnecting: (connecting: boolean) => void;
   setUserPublicKey: (publicKey: string) => void;
   setUserMinaBalance: (balance: number) => void;
   setWishlist: (wishlist: number[]) => void;
   addWishlist: (gameId: number) => void;
   removeWishlist: (gameId: number) => void;
-  // setFlag: () => void;
-  // nullifyFlag: () => void;
   setLibrary: (library: number[]) => void;
   disconnect: () => void;
 }
 
 export const useUserStore = create<UserState>()((set) => ({
   isConnected: false,
-  isConnecting: false,
   userPublicKey: '',
   userMinaBalance: 0,
   wishlist: [],
-  // wishlistFlag: false,
   library: [],
 
   setConnected: (connected) => set({ isConnected: connected }),
-  setConnecting: (connecting) => set({ isConnecting: connecting }),
   setUserPublicKey: (publicKey) => set({ userPublicKey: publicKey }),
   setUserMinaBalance: (balance) => set({ userMinaBalance: balance }),
   addWishlist: (gameId) =>
@@ -42,13 +34,10 @@ export const useUserStore = create<UserState>()((set) => ({
       wishlist: state.wishlist.filter((id) => id !== gameId),
     })),
   setWishlist: (wishlist) => set({ wishlist }),
-  // setFlag: () => set({ wishlistFlag: true }),
-  // nullifyFlag: () => set({ wishlistFlag: false }),
   setLibrary: (library) => set({ library }),
   disconnect: () =>
     set({
       isConnected: false,
-      isConnecting: false,
       userPublicKey: '',
       userMinaBalance: 0,
       wishlist: [],
