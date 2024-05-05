41b31e93cf9dd499d8cf69095ce1743e12566d60
kadirchan <kadircanbozkurt1905@gmail.com>
2024-05-05 16:21:48 +0300

    define Game interface

---
+++ src/renderer/lib/globals.d.ts
@@ -0,0 +1,12 @@
+interface Game {
+  gameId: number;
+  name: string;
+  description: string;
+  creator: string;
+  cover: string;
+  price: number;
+  discount: number;
+  rating: number;
+  releaseDate: string;
+  tags: string[];
+}
