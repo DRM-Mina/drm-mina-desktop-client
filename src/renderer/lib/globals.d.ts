2b44cda982e1eb288b134aa5e904999d1afb4f64
kadirchan <kadircanbozkurt1905@gmail.com>
2024-05-05 19:12:38 +0300

    add query interfaces

--- src/renderer/lib/globals.d.ts
+++ src/renderer/lib/globals.d.ts
@@ -10,3 +10,40 @@ interface Game {
   releaseDate: string;
   tags: string[];
 }
+
+interface GamePrices {
+  data: {
+    runtime: {
+      GameToken: {
+        gamePrice: {
+          value: string | null;
+        };
+        discount: {
+          value: string | null;
+        };
+      };
+    };
+  };
+}
+
+interface GameNumber {
+  data: {
+    runtime: {
+      GameToken: {
+        totalGameNumber: {
+          value: string | null;
+        };
+      };
+    };
+  };
+}
+
+interface Library {
+  data: {
+    runtime: {
+      GameToken: {
+        users: boolean | null;
+      };
+    };
+  };
+}
