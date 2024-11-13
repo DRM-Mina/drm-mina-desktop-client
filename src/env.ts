aa5e0fd41554af8b7f56a87d254e04b7df643cd0
kadirchan <kadircanbozkurt1905@gmail.com>
2024-11-14 02:08:01 +0300

    update endpoints

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
-export const API_URL = 'https://api_drmmina.kadircan.org/';
+export const API_URL = 'https://api.drmmina.com/';
 export const WEB_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/'
-    : 'https://drm-mina.kadircan.org/';
+    : 'https://drmmina.com/';
